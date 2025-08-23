0f025caeadf134510e8672ba463d342b8f019104
Asher Samuel <ashersam116@gmail.com>
2025-08-23 16:56:30 +0300

    constraints

---
+++ internal/headers/headers.go
@@ -0,0 +1,99 @@
+package headers
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+)
+
+var rn = []byte("\r\n")
+
+func isToken(str []byte) bool {
+	for _, ch := range str {
+		found := false
+		if ch >= 'A' && ch <= 'Z' ||
+			ch >= 'a' && ch <= 'z' ||
+			ch >= '0' && ch <= '9' {
+			found = true
+		}
+
+		switch ch {
+		case '#', '$', '%', '&', '\'', '*', '+', '-', '.', '^', '_', '`', '|', '~':
+			found = true
+		}
+
+		if !found {
+			return false
+		}
+	}
+
+	return true
+}
+
+type Headers struct {
+	headers map[string]string
+}
+
+func NewHeaders() *Headers {
+	return &Headers{
+		headers: map[string]string{},
+	}
+}
+
+func parseHeader(fieldLine []byte) (string, string, error) {
+	parts := bytes.SplitN(fieldLine, []byte(":"), 2)
+	if len(parts) != 2 {
+		return "", "", fmt.Errorf("malformed field line")
+	}
+
+	name := parts[0]
+	value := bytes.TrimSpace(parts[1])
+
+	if bytes.HasSuffix(name, []byte(" ")) {
+		return "", "", fmt.Errorf("malformed field name")
+	}
+
+	return string(name), string(value), nil
+}
+
+func (h *Headers) Get(name string) string {
+	return h.headers[strings.ToLower(name)]
+}
+
+func (h *Headers) Set(name, value string) {
+	h.headers[strings.ToLower(name)] = value
+}
+
+// change the named return values
+func (h *Headers) Parse(data []byte) (int, bool, error) {
+	read := 0
+	done := false
+
+	for {
+		idx := bytes.Index(data[read:], rn)
+		if idx == -1 {
+			break
+		}
+
+		// empth header
+		if idx == 0 {
+			done = true
+			read += len(rn)
+			break
+		}
+
+		name, value, err := parseHeader(data[read : read+idx])
+		if err != nil {
+			return 0, false, err
+		}
+
+		if !isToken([]byte(name)) {
+			return 0, false, fmt.Errorf("malformed header name")
+		}
+
+		read += idx + len(rn)
+		h.Set(name, value)
+	}
+
+	return read, done, nil
+}
